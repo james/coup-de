9109842548e04ecb86fcea33f2d8f20a5540bd7e
James Darling <james@abscond.org>
2008-11-16 15:58:57 +0000

    flickr

--- site/javascripts/application.js
+++ site/javascripts/application.js
@@ -23,10 +23,10 @@ GitHub = $.klass({
 });
 
 LastFmEvents = $.klass({
-  initialize: function(user_id) {
+  initialize: function(user_id, api_key) {
     var element = jQuery('<ul></ul>');
     this.element.replaceWith(element);
-    $.getJSON("http://lastfm-api-ext.appspot.com/2.0/?method=user.getevents&user="+user_id+"&api_key=ef5f6b42e168116f913ed26eeacb7e34&outtype=js&callback=?", function(data){ 
+    $.getJSON("http://lastfm-api-ext.appspot.com/2.0/?method=user.getevents&user="+user_id+"&api_key="+api_key+"&outtype=js&callback=?", function(data){ 
       $.each(data.events, function(i, item) { 
         element.append("<li>"+item.title+"</li>");
       });
@@ -34,9 +34,27 @@ LastFmEvents = $.klass({
   }
 });
 
+Flickr = $.klass({
+  initialize: function(user_id) {
+    var element = jQuery('<ul></ul>');
+    this.element.replaceWith(element);
+    $.getJSON("http://api.flickr.com/services/feeds/photos_public.gne?id="+user_id+"&format=json&jsoncallback=?", function(data){ 
+      $.each(data.items, function(i, item) { 
+        element.append(
+          '<li>'+
+            '<a href="'+item.link+'">'+
+              '<img src="'+item.media.m.replace(/_m.jpg$/, "_s.jpg")+'" alt="'+item.title+' />'+
+            '</a>'+
+          '</li>'
+        );
+      });
+    });
+  }
+});
 
 jQuery(function($) {
   $('a.twitter').attach(Twitter, 'coupde');
   $('a.github').attach(GitHub, 'james');
-  $('a.gigs').attach(LastFmEvents, 'Abscond');
+  $('a.gigs').attach(LastFmEvents, 'Abscond', 'ef5f6b42e168116f913ed26eeacb7e34');
+  $('a.photos').attach(Flickr, '82586441@N00');
 });
