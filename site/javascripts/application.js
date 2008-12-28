bdbc1a6ac19c67748ad9c3bb7a30dc77b716a9bf
James Darling <james@abscond.org>
2008-12-28 16:45:15 +0000

    slidey slidey. It's nice to not have things dissapearing and appearing unnaturally

--- site/javascripts/application.js
+++ site/javascripts/application.js
@@ -1,28 +1,45 @@
 console.info();
 Nav = $.klass({
   initialize: function() {
-    this.setDefaultCurrent();
-    this.hideAllButCurrent();
+    // Set the first item as current if not already set
+    if(!this.element.find('.current')[0]) {
+      this.setCurrent(this.element.find('a:first'));
+    }
+    
+    // Hide all non-current sections
+    $('.section[id!='+this.currentSection()+']').hide();
   },
+  
   onclick: $.delegate({
     'a': function(link) {
-      this.element.find('.current').removeClass('current');
-      link.addClass('current');
-      this.hideAllButCurrent();
+      this.setCurrent(link);
+      this.sectionForLink(this.last).hide("slide", { direction: "right" }, 500);
+      this.sectionForLink(link).show("slide", { direction: "left" }, 500);
       return false;
     }
   }),
-  setDefaultCurrent: function() {
-    if(!this.element.find('.current')[0]) {
-      this.element.find('a:first').addClass('current');
+  
+  currentLink: function() {
+    if(this.element.find('.current').size() > 0) {
+      return this.element.find('.current');
+    } else {
+      return false
     }
   },
-  hideAllButCurrent: function() {
-    $('.section[id!='+this.current()+']').hide();
-    $('#'+this.current()).show();
+  currentSection: function() {
+    if(this.currentLink()) {
+      return this.currentLink().attr("href").slice(1,1000);
+    } else {
+      return false
+    }
   },
-  current: function() {
-    return this.element.find('.current').attr("href").slice(1,1000);
+  setCurrent: function(link) {
+    if(this.currentLink()) {this.last = this.currentLink();}
+    this.element.find('.current').removeClass('current');
+    link.addClass('current');
+  },
+  sectionForLink: function(link) {
+    return $(link.attr("href"))
   }
 });
 
